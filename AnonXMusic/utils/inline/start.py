5545d9791c9dd4e582da9f87c7d56529e9cffb46
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-08-10 08:34:25 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -12,7 +12,6 @@ def start_panel(_):
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
     
     ]
     return buttons
